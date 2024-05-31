f158dee296b0e6506c8632149ebbe7099239c439
Aymen <6239044@bdeb.qc.ca>
2024-05-30 23:08:57 -0400

    modification des pages recherches car problème d'icone

--- TP3/jvs/recherche_dessert.js
+++ TP3/jvs/recherche_dessert.js
@@ -1,3 +1,12 @@
-function rechercheP(){
-    
+function rechercheD() {
+    const url = `http://localhost:8080/ords/restscott/desserts`; 
+
+        fetch(url)
+            .then((resp) => resp.json())
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.log(JSON.stringify(error));
+            });
 }
--- TP3/jvs/recherche_plat.js
+++ TP3/jvs/recherche_plat.js
@@ -1,3 +1,12 @@
 function rechercheP(){
-    
+    const url = `http://localhost:8080/ords/restscott/plats_principaux`; 
+
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log(JSON.stringify(error));
+        });
 }
