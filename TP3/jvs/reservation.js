6d0e23f4f7b8854c0e884046943ce2707993d736
Aymen <6239044@bdeb.qc.ca>
2024-05-31 10:25:58 -0400

    modification des liens des pages

--- TP3/jvs/reservation.js
+++ TP3/jvs/reservation.js
@@ -1,16 +1,12 @@
 function reponse(event) {
-    // Prevent the default form submission behavior
     event.preventDefault();
 
-    // Show a window alert
     window.alert("Réservation enregistrée avec succès!");
 
-    // Get the form data
     const date = document.getElementById("form-control").value;
     const heure = document.getElementById("Heure").value;
     const nombrePersonnes = document.getElementById("nombre-personnes").value;
 
-    // Log the form data to the console
     console.log("Date:", date);
     console.log("Heure:", heure);
     console.log("Nombre de personnes:", nombrePersonnes);
